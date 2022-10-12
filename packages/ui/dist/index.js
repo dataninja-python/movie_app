8751f726345b9174b9b1ebc3cb679315c3b2c78d
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-11 20:12:30 -0400

    theme working

--- packages/ui/dist/index.js
+++ packages/ui/dist/index.js
@@ -1,5 +1,4 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { AppShell as MantineAppShell, Title, Header, MantineProvider } from '@mantine/core';
-export const AppShell = ({ title, colorScheme }) => (_jsx(MantineProvider, Object.assign({ withGlobalStyles: true, withNormalizeCSS: true, theme: {
-        colorScheme,
-    } }, { children: _jsxs(MantineAppShell, { children: [_jsx(Header, Object.assign({ height: 60, p: "xs", sx: { display: "flex" } }, { children: _jsx(Title, { children: title }) })), _jsx("div", { children: "Hello" })] }) })));
+import { BrowserRouter } from 'react-router-dom';
+export const AppShell = ({ title, colorScheme }) => (_jsx(BrowserRouter, { children: _jsx(MantineProvider, Object.assign({ withGlobalStyles: true, withNormalizeCSS: true, theme: { colorScheme, } }, { children: _jsxs(MantineAppShell, { children: [_jsx(Header, Object.assign({ height: 60, p: "xs", sx: { display: "flex" } }, { children: _jsx(Title, { children: title }) })), _jsx("div", { children: "Hello" })] }) })) }));
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -10,19 +10,10 @@ export const AppShell: React.FunctionComponent<{
         <MantineProvider
             withGlobalStyles
             withNormalizeCSS
-            theme={{
-                colorScheme,
-            }}
+            theme={{ colorScheme, }}
         >
             <MantineAppShell>
-                <Header height={60} p="xs" sx={{ display: "flex" }}
-                    styles={
-                        (theme) => ({
-                            main: {
-                                backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
-                            },
-                        })}
-                >
+                <Header height={60} p="xs" sx={{ display: "flex" }}>
                     <Title>{title}</Title>
                 </Header>
 
